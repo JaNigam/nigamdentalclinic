19d4bd510c355a2cdbff251dcd4b23514ac2ac83
Jayant Nigam <jayantnigam9@gmail.com>
2022-09-15 16:11:15 +0530

    release-6: Home slider images & autoplay

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 //responsive navigation bar
 
 
-
-
-
 var swiper = new Swiper(".swiper-1", {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -13,6 +10,8 @@ var swiper = new Swiper(".swiper-1", {
         clickable: true,
 
     },
+
+
     breakpoints: {
         640: {
             slidesPerView: 1,
@@ -28,6 +27,57 @@ var swiper = new Swiper(".swiper-1", {
         },
     },
 });
+
+
+
+var swiper = new Swiper(".swiper-2", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+
+    },
+
+    autoplay: {
+    delay:2000,
+    },
+
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            // spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 1,
+            // spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            // spaceBetween: 50,
+        },
+    },
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
 
 
 //form testing
